4fd52740f8f7653d8a380e84199dd11b120278d6
JyothiKarumuri <143922684+JyothiKarumuri05@users.noreply.github.com>
2025-05-17 15:40:58 +0530

    Task2_17_05_2025.py

--- Task2_17_05_2025.py
+++ Task2_17_05_2025.py
@@ -1,5 +1,4 @@
-# Write a script to calculate the area and perimeter 
-# of a rectangle using variables. 
+# Write a script to calculate the area and perimeter of a rectangle using variables. 
 length = 10
 width = 5
 area = length * width
@@ -8,9 +7,7 @@ print("Area of the rectangle:", area)
 print("Perimeter of the rectangle:", perimeter)
 
 
-# Write a script that takes two numbers as input and prints 
-# whether the first number
-#  is greater than, less than, or equal to the second number.
+# Write a script that takes two numbers as input and prints  whether the first number is greater than, less than, or equal to the second number.
 num1 = int(input("Enter the first number: "))
 num2 = int(input("Enter the second number: "))
 if num1 > num2:
@@ -20,8 +17,7 @@ elif num1 < num2:
 else:
     print("Both numbers are equal.")
 
-# Write a script that checks if a given year is a leap year (divisible by 
-# 4, but not by 100 unless also divisible by 400). 
+# Write a script that checks if a given year is a leap year (divisible by 4, but not by 100 unless also divisible by 400). 
 
 year = int(input("Enter a year: "))
 
@@ -31,8 +27,21 @@ else:
     print(year, "is not a leap year.")
 #Experiment with different arithmetic and logical operators in the interpreter. 
 
-
-
+>>>4+5
+9
+>>>9*3
+27
+>>>2**2
+4
+>>>10/5
+2
+>>> not(4>3)
+False
+>>> (5>8 and 6<7)
+False
+>>> 7>5 or 6>7
+True
+>>>exit()
 
 # Write a script that concatenates two strings and prints the result.
 str1="Jyothi"
