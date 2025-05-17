dbb0e9714d66b0710a85c47f71e3c7264ca9152a
JyothiKarumuri <143922684+JyothiKarumuri05@users.noreply.github.com>
2025-05-17 15:32:25 +0530

    Add files via upload

---
+++ Task1_17_05_2025.py
@@ -0,0 +1,27 @@
+# Write a simple Python script that prints your name and a greeting.
+print("Hello, my name is Jyothi Karumuri")
+print("Nice to meet you!")
+
+
+# Experiment with running Python code directly in the interpreter. 
+
+
+print("Hello")
+2+10*5
+52
+
+
+# Write a script with comments explaining different parts of the code
+
+# Printing a greeting message
+print("Hello, world!")
+
+# Declaring a variable to store  name
+my_name = "Jyothi"
+
+# Printing  name 
+print("My name is", my_name)
+
+# Adding two numbers just for fun
+sum_result = 5 + 10  # This adds 5 and 10
+print("5 + 10 =", sum_result)
